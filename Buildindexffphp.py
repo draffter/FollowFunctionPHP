cd01cc6a0ec53b8de3d819e5e045ef7349bef627
sn0 <sn0@holicon.pl>
2013-01-11 12:54:38 +0100

    dodane indeksowanie na wątkach zmiana mapowania klawiszy

--- Buildindexffphp.py
+++ Buildindexffphp.py
@@ -2,8 +2,52 @@ import sublime, sublime_plugin
 import tempfile, os, sys
 import fnmatch
 import re
+import threading
+
 
 class BuildindexffphpCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+		threads = []
+		dirs = self.getDirectories()
+		self.dirCount = len(dirs)
+		for dir in dirs:
+			pName = self.getDirectoryMD5(dir)
+			dbPath = os.path.join(sublime.packages_path(), "Followfunctionphp", pName)
+			thread = BuildindexThread(dir, dbPath)
+			threads.append(thread)
+			thread.start()
+		sublime.status_message("Indeksujemy!")
+		self.handle_threads(threads)
+
+	def handle_threads(self, threads):
+		next_threads = []
+		for thread in threads:
+			if thread.is_alive():
+				next_threads.append(thread)
+				continue
+			if thread.result == False:
+				continue
+		threads = next_threads
+		if (len(threads)):
+			sublime.status_message("Indeksacja katalogow PHP. Pozostalo: %d" % (self.dirCount - (self.dirCount - len(threads))))
+			sublime.set_timeout(lambda: self.handle_threads(threads), 200)
+		else:
+			sublime.status_message("Koniec indeksacji")
+
+		return
+
+	def getDirectories(self):
+		return sublime.active_window().folders()
+
+	def getDirectoryMD5(self, dir):
+		import hashlib
+		m = hashlib.md5()
+		m.update(dir)
+		md5var = m.hexdigest()
+		return md5var
+
+class BuildindexThread(threading.Thread):
 	functionsArray = []
 	dbFile = None
 
@@ -52,22 +96,14 @@ class BuildindexffphpCommand(sublime_plugin.TextCommand):
 		except IOError:
 			print "blad zamkniecia "
 
-	def run(self, edit):
-		dirs = self.getDirectories()
-		for dir in dirs:
-			pName = self.getDirectoryMD5(dir)
-			dbPath = os.path.join(sublime.packages_path(), "Followfunctionphp", pName)
-			if self.initDb(dbPath):
-				self.find(dir, " function ", "*.php")
-				self.closeDb()
+	def __init__(self, dir, dbPath):
+		self.dir = dir
+		self.dbPath = dbPath
+		self.result = None
+		threading.Thread.__init__(self)
+
+	def run(self):
+		if self.initDb(self.dbPath):
+			self.find(self.dir, " function ", "*.php")
+			self.closeDb()
 		return
-
-	def getDirectories(self):
-		return sublime.active_window().folders()
-
-	def getDirectoryMD5(self, dir):
-		import hashlib
-		m = hashlib.md5()
-		m.update(dir)
-		md5var = m.hexdigest()
-		return md5var
